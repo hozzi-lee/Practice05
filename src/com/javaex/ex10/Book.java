89274cbd55c522eb4fc5c994a6604218c62c871f
leehojin-93 <hogenie93@gmail.com>
2021-06-04 23:29:01 +0900

    21. 6. 4. 금) 23:28 미완성 update

---
+++ src/com/javaex/ex10/Book.java
@@ -0,0 +1,81 @@
+package com.javaex.ex10;
+
+public class Book {
+
+	// 필드
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+
+
+	// 생성자
+	public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+	}
+
+	public Book(int stateCode) {
+		this.stateCode = 1;
+	}
+
+
+
+	// 메소드 게터세터
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+
+	// 메소드 일반
+	/*
+	public void displayBookInfo(Book[] books) {
+		for ( int i = 0; i < books.length; i++ ) {
+			System.out.println(books[i]);
+		}
+	}
+	 */	
+
+
+	public void rent() {
+		this.stateCode = 0;
+		System.out.println(getTitle() + "이(가) 대여 됐습니다");
+	}
+
+	public void print(Book[] books) {
+		for ( int i = 0; i < books.length; i++ ) {
+			if ( stateCode == 1 ) {
+				System.out.println(books[i] + "대여 유무: 재고있음");
+			} else {
+				System.out.println(books[i] + "대여 유무: 대여중");
+			}
+		}
+	}
+
+}
